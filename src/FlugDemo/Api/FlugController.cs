7135e72307ed9dd35c32996ad633a5c8440e9a83
Manfred Steyer <manfred.steyer@gmx.net>
2016-03-04 12:19:33 +0100

    first commit

---
+++ src/FlugDemo/Api/FlugController.cs
@@ -0,0 +1,52 @@
+﻿using FlugDemo.Components;
+using FlugDemo.Data;
+using FlugDemo.Models;
+using Microsoft.AspNet.Authorization;
+using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlugDemo.Api
+{
+    [Route("api/[controller]")]
+    // [Authorize]
+    public class FlugController: Controller
+    {
+        private IFlugRepository repo;
+
+        public FlugController(IFlugRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        // GET api/flug
+        [HttpGet]
+        public List<Flug> GetAll() {
+            return repo.FindAll();
+        }
+
+        // api/flug/byRoute
+        [HttpGet("byRoute")]
+        public List<Flug> GetByRoute(string von, string nach) {
+            return repo.FindByRoute(von, nach);
+        }
+
+        // api/flug/{id}
+        [HttpGet("{id}")]
+        public Flug GetById(int id) {
+            return repo.FindById(id);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Flug flug) {
+            repo.Save(flug);
+            // Status 201
+            // return CreatedAtAction("GetById", new { id = flug.Id }, flug);
+
+            return new AcceptedActionResult();
+        }
+
+    }
+}
---
+++ src/FlugDemo/Startup.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using Microsoft.Extensions.DependencyInjection;
+using FlugDemo.Data;
+using Microsoft.AspNet.Mvc.Formatters;
+using Newtonsoft.Json;
+using Microsoft.Extensions.Configuration;
+using System.Diagnostics;
+
+namespace FlugDemo
+{
+    // ASP.NET Core 1
+    public class Startup
+    {
+        IConfiguration Configuration;
+
+        public Startup(IHostingEnvironment env)
+        {
+            // Set up configuration sources.
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json");
+
+            if (env.IsDevelopment())
+            {
+                // dnu commands install Microsoft.Extensions.SecretManager
+                // user-secret set [key] [value]
+
+                builder.AddUserSecrets();
+            }
+
+            Configuration = builder.Build();
+
+            var secureQueryString = Configuration["SecureConnectionString"];
+
+            Debug.WriteLine(secureQueryString);
+
+        }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen();
+            services.AddSignalR();
+
+            // StructureMap
+
+            services.AddMvc().AddMvcOptions(options => {
+
+                var inFormatters = options.InputFormatters;
+                var outFormatters = options.OutputFormatters;
+
+
+
+                #region xml
+                
+                var xmlInput = new XmlDataContractSerializerInputFormatter();
+                var xmlOutput = new XmlDataContractSerializerOutputFormatter();
+
+                options.InputFormatters.Add(xmlInput);
+                options.OutputFormatters.Add(xmlOutput);
+                
+                #endregion
+
+            }).AddJsonOptions(options => {
+
+                #region options
+                options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                options.SerializerSettings.Formatting = Formatting.Indented;
+                #endregion
+
+            });
+
+            services.AddTransient<IFlugRepository, FlugEfRepository>();
+
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseIISPlatformHandler();
+
+            /*
+            app.UseJwtBearerAuthentication(options =>
+            {
+                options.AutomaticAuthenticate = true;
+                options.Authority = "https://steyer-identity-server.azurewebsites.net/identity";
+                options.Audience = "https://steyer-identity-server.azurewebsites.net/identity/resources";
+            });
+            */
+
+            app.UseWebSockets();
+
+            var origins = new[] { "http://localhost:8887" };
+            app.UseCors(config => config.WithOrigins(origins).AllowAnyMethod().AllowAnyHeader().AllowCredentials());
+
+            if (env.IsDevelopment()) {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else {
+                app.UseExceptionHandler("/home/error");
+            }
+
+            app.UseSwaggerGen(); // /swagger
+            app.UseSwaggerUi(); // /swagger/ui
+
+            app.UseStaticFiles();
+
+            app.UseSignalR();
+
+            app.UseMvc( routes => {
+                routes.MapRoute(
+                        "default",
+                        "{controller=Home}/{action=Index}/{id?}");
+            });
+            
+
+        }
+
+        // Entry point for the application.
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
